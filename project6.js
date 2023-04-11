9428f3c60404c37c435f064efc0fbf7caa2ad540
AbdallahAbdrabo <127974076+AbdallahAbdrabo@users.noreply.github.com>
2023-04-11 04:06:33 +0200

    Add files via upload

---
+++ project6.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('#minu1');
+let navlist = document.querySelector('.minu');
+
+menu.onclick = () => {
+   
+    // menu.classList.toggle('minu1');
+    navlist.classList.toggle('open');
+
+}
+function close1(){
+
+    
+}
